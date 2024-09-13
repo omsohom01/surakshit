f5dc4dd536e8a4006b520e5460a9ff4c362868c3
omsohom01 <omsohom01@gmail.com>
2024-09-13 23:25:22 +0530

    Update server.py

--- server.py
+++ server.py
@@ -44,6 +44,5 @@ def send_alert():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    # Bind to the port provided by Render
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)  
